fd49caa4f7219cdf59c1e7a6a7ade78805e47ac6
ajayfaul <ajayfaul@ajayfaul-desktop>
2017-11-14 16:30:13 +0700

    Model Mahasiswa

---
+++ src/model/ModelMahasiswa.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ajayfaul
+ * Kelas :IF-11
+ * 10116490 / Muhammad Fajar Maulana
+ */
+public class ModelMahasiswa {
+public String nim;
+    public String nama;
+    
+    public ModelMahasiswa(String nim, String nama){
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+      
+
+         
+    } 
+}
